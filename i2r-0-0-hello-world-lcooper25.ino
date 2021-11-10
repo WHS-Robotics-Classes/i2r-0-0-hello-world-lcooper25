ea855b9df8eded38674f6e1f549d38fdd37b6624
lcooper25 <90796430+lcooper25@users.noreply.github.com>
2021-11-10 14:00:53 -0700

    Create i2r-0-0-hello-world-lcooper25.ino

---
+++ i2r-0-0-hello-world-lcooper25.ino
@@ -0,0 +1,11 @@
+
+void setup() {
+  Serial.begin(9600);// put your setup code here, to run once:
+   // put your message that you would like to display in the serial monitor in quotes inside the parenthesis here
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Serial.println("hello world" );
+}
